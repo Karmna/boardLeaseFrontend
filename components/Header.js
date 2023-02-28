86584a0d89535dce268f547d89c84bc790e3b03c
Ambre <demeure.ambre@gmail.com>
2023-02-28 10:40:53 +0100

    HeaderGoogle

--- components/Header.js
+++ components/Header.js
@@ -5,10 +5,12 @@ import * as React from 'react';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { faUser, faXmark, faBars} from '@fortawesome/free-solid-svg-icons';
+import { faUser, faXmark, faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { DownOutlined, UserOutlined, EyeInvisibleOutlined, EyeTwoTone  } from '@ant-design/icons';
+import { DownOutlined, UserOutlined, EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
 import { Button, Dropdown, message, Space, Tooltip, Input, Password } from 'antd';
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+import jwtDecode from 'jwt-decode';
 
 function Header() {
     const matches = useMediaQuery('(min-width:468px)')
@@ -21,7 +23,11 @@ function Header() {
     const [signUpLastname, setSignUpLastname] = useState('');
     const [signUpFirstname, setSignUpFirstname] = useState('');
     const [signUpMail, setSignUpMail] = useState('');
-    const [passwordVisible, setPasswordVisible] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [userGoogle, setUserGoogle] = useState(null);
+
+
+    const clientId = '755080318307-4oa4og0udgb1vt0s4cd95tto75hcmnqo.apps.googleusercontent.com';
 
     const [signInUsername, setSignInUsername] = useState('');
     const [signInPassword, setSignInPassword] = useState('');
@@ -33,6 +39,10 @@ function Header() {
     };
     const showModalConnection = () => {
         setIsModalVisibleConnection(!isModalVisibleConnection);
+    };
+
+    const handleLogin = (credentialResponse) => {
+        setUserGoogle(jwtDecode(credentialResponse.credential));
     };
 
     const items = [
@@ -59,7 +69,7 @@ function Header() {
         onClick: handleMenuClick,
     };
     const handleRegister = () => {
-        fetch('http://localhost:3000/users/signup',{
+        fetch('http://localhost:3000/users/signup', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ firstname: signUpFirstname, lastname: signUpLastname, username: signUpUsername, email: signUpMail, password: signUpPassword }),
@@ -81,7 +91,7 @@ function Header() {
     };
 
     const handleConnection = () => {
-        fetch('http://localhost:3000/users/signin',{
+        fetch('http://localhost:3000/users/signin', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ username: signInUsername, password: signInPassword }),
@@ -96,122 +106,104 @@ function Header() {
             });
 
     }
-    // const handleLogout = () => {
-    //     dispatch(logout());
-    // };
-
 
 
     let modalContentInscription;
     if (!user.token) {
         modalContentInscription = (
             <div className={styles.registerContainer}>
-                <div className={styles.registerSection}>
-                    <p>S'enregistrer</p>
-                    <Input type="Prénom" placeholder="Prénom" id="signUpFirstname" onChange={(e) => setSignUpFirstname(e.target.value)} value={signUpFirstname} />
-                    <Input type="Nom" placeholder="Nom" id="signUpLastname" onChange={(e) => setSignUpLastname(e.target.value)} value={signUpLastname} />
-                    <Input type="Nom d'utilisateur" placeholder="Nom d'utilisateur" id="signUpUsername" onChange={(e) => setSignUpUsername(e.target.value)} value={signUpUsername} />
-                    <Input type="Email" placeholder="Email" id="Email" onChange={(e) => setSignUpMail(e.target.value)} value={signUpMail} />
-                    <Input.Password
-       placeholder="Mot de passe" id="signUpPassword" onChange={(e) => setSignUpPassword(e.target.value)} value={signUpPassword}
-        iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
-      />
-                    <Button id="register" onClick={() => handleRegister()}>S'enregistrer</Button>
-                </div>
+                <GoogleOAuthProvider clientId={clientId}>
+                    <div className={styles.container}>
+                        {userGoogle ?
+                            <div className={styles.content}>
+                                <div className={styles.divider}></div>
+                                <p>Email: {userGoogle.email}</p>
+                            </div>
+                            : <div className={styles.content}>
+                                <Input type="Prénom" placeholder="Prénom" id="signUpFirstname" onChange={(e) => setSignUpFirstname(e.target.value)} value={signUpFirstname} />
+                                <Input type="Nom" placeholder="Nom" id="signUpLastname" onChange={(e) => setSignUpLastname(e.target.value)} value={signUpLastname} />
+                                <Input type="Nom d'utilisateur" placeholder="Nom d'utilisateur" id="signUpUsername" onChange={(e) => setSignUpUsername(e.target.value)} value={signUpUsername} />
+                                <Input type="Email" placeholder="Email" id="Email" onChange={(e) => setSignUpMail(e.target.value)} value={signUpMail} />
+                                <Input.Password
+                                    placeholder="Mot de passe" id="signUpPassword" onChange={(e) => setSignUpPassword(e.target.value)} value={signUpPassword}
+                                    iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+                                />
+                                <Button id="register" onClick={() => handleRegister()}>S'enregistrer</Button>
+                                <h2>Se connecter avec Google</h2>
+                                <div className={styles.divider}></div>
+                                <GoogleLogin
+                                    onSuccess={(credentialResponse) => handleLogin(credentialResponse)}
+                                    onError={((error) => console.error(error))}
+                                />
+                            </div>}
+                    </div>
+                </GoogleOAuthProvider>
             </div>
         );
     }
 
-    // let userSectionInscription;
-    // if (user.token) {
-    //     userSectionInscription = (
-    //         <div className={styles.logoutSection}>
-
-    //             <button onClick={() => handleLogout()}>Logout</button>
-    //         </div>
-    //     );
-    // } else {
-    //     if (isModalVisibleInscription) {
-    //         userSectionInscription =
-    //             <div className={styles.headerIcons}>
-    //                 <FontAwesomeIcon onClick={showModalInscription} className={styles.userSection} icon={faXmark} />
-    //             </div>
-    //     } else {
-    //         userSectionInscription =
-    //             <div className={styles.headerIcons}>
-    //                 <FontAwesomeIcon onClick={showModalInscription} className={styles.userSection} />
-    //             </div>
-    //     }
-    // }
 
     let modalContentConnection;
     if (!user.token) {
         modalContentConnection = (
             <div className={styles.registerContainer}>
-                <div className={styles.registerSection}>
-                    <p>Se connecter</p>
-                    <Input type="text" placeholder="Nom d'utilisateur" id="signInUsername" onChange={(e) => setSignInUsername(e.target.value)} value={signInUsername} />
-                    <Input.Password
-       placeholder="Mot de passe" id="signInPassword" onChange={(e) => setSignInPassword(e.target.value)} value={signInPassword}
-        iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
-      />
-                    <Button id="connection" onClick={() => handleConnection()}>Se connecter</Button>
-                </div>
+                <GoogleOAuthProvider clientId={clientId}>
+                    <div className={styles.container}>
+                        {userGoogle ?
+                            <div className={styles.content}>
+                                <div className={styles.divider}></div>
+                                <p>Email: {userGoogle.email}</p>
+                            </div>
+                            :
+                            <div className={styles.registerSection}>
+                                <Input type="text" placeholder="Nom d'utilisateur" id="signInUsername" onChange={(e) => setSignInUsername(e.target.value)} value={signInUsername} />
+                                <Input.Password
+                                    placeholder="Mot de passe" id="signInPassword" onChange={(e) => setSignInPassword(e.target.value)} value={signInPassword}
+                                    iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+                                />
+                                <Button id="connection" onClick={() => handleConnection()}>Se connecter</Button>
+                                <h2>Se connecter avec Google</h2>
+                                <div className={styles.divider}></div>
+                                <GoogleLogin
+                                    onSuccess={(credentialResponse) => handleLogin(credentialResponse)}
+                                    onError={((error) => console.error(error))}
+                                />
+                            </div>}
+                    </div>
+                </GoogleOAuthProvider>
             </div>
+
         );
     }
 
-    // let userSectionConnection;
-    // if (user.token) {
-    //     userSectionConnection = (
-    //         <div className={styles.logoutSection}>
-    //             <button onClick={() => handleLogout()}>Logout</button>
-    //         </div>
-    //     );
-    // } else {
-    //     if (isModalVisibleConnection) {
-    //         userSectionConnection =
-    //             <div className={styles.headerIcons}>
-    //                 <FontAwesomeIcon onClick={showModalConnection} className={styles.userSection} icon={faXmark} />
-    //             </div>
-    //     } else {
-    //         userSectionConnection =
-    //             <div className={styles.headerIcons}>
-    //                 <FontAwesomeIcon onClick={showModalConnection} className={styles.userSection} icon={faUser} />
-    //             </div>
-    //     }
-    // }
 
 
     return (
+
         <header className={styles.header}>
+
             <div className={styles.logoContainer}>
                 <img className={styles.logo} src="logo.svg" alt="Logo" />
             </div>
+
             <div className={styles.dropDownContainer}>
-            <Dropdown.Button menu={menuProps} placement="bottomRight" icon={<UserOutlined />} > <FontAwesomeIcon icon={faBars} />
+                <Dropdown.Button menu={menuProps} placement="bottomRight" icon={<UserOutlined />} > <FontAwesomeIcon icon={faBars} />
+                    <div id="react-modals">
 
-                {isModalVisibleInscription && <div id="react-modals">
-               
-                    <Modal getContainer="#react-modals" className={styles.modal} open={isModalVisibleInscription} closable={false} footer={null}>
-                        {modalContentInscription}
-                        <FontAwesomeIcon className={styles.userSection} icon={faXmark} />
-                    </Modal>
-                </div>}
+                        <Modal title="Inscription" className={styles.modal} open={isModalVisibleInscription} closable={false} footer={null} >
+                            <p>{modalContentInscription}</p>
 
-                {isModalVisibleConnection && <div id="react-modals">
-                    <Modal getContainer="#react-modals" className={styles.modal} open={isModalVisibleConnection} closable={false} footer={null}>
-                        {modalContentConnection}
-                        <FontAwesomeIcon className={styles.userSection} icon={faXmark} />
-                    </Modal>
-                </div>}
+                        </Modal>
 
+                    </div>
 
-            </Dropdown.Button>
+                    <div id="react-modals">
+                        <Modal title="Connection" className={styles.modal} open={isModalVisibleConnection} closable={false} footer={null}>
+                            <p>{modalContentConnection}</p>
+                        </Modal>
+                    </div>
+                </Dropdown.Button>
             </div>
-
-
-
 
         </header >
     );
