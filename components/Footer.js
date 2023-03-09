5400a15f56d5a92ad2e8dbc1bd9cdbed19ce80a9
Antony Merle <antony.merle@gmail.com>
2023-03-09 13:03:46 +0100

    fignolage footer

--- components/Footer.js
+++ components/Footer.js
@@ -6,55 +6,48 @@ import Link from "next/link";
 import { Layout } from "antd";
 const { Header, Content, Footer } = Layout;
 
-
 // test deploy
 
 function FooterFunction() {
   const matches = useMediaQuery("(min-width:768px)");
 
   const footerDisplay = !matches ? (
-   
-      <footer className={styles.footerStyle}>
-        <div className={styles.footerlogoExplore}>
-          <Link href="/explorer">
-            <FontAwesomeIcon
-              className={styles.useSelector}
-              icon={faMagnifyingGlass}
-            />
-          </Link>
-          <br/>
-          <br/>
-          <p>Explorer</p>
-        </div>
-        <div className={styles.footerlogoCoeur}>
-          <Link href="/favorites">
-            <FontAwesomeIcon className={styles.useSelector} icon={faHeart} />
-          </Link>
-          <br/>
-          <br/>
-          <p>Favoris</p>
-        </div>
-      </footer>
-    
+    <footer className={styles.footerStyle}>
+      <div className={styles.footerlogoExplore}>
+        <Link href="/explorer">
+          <FontAwesomeIcon
+            className={styles.useSelector}
+            icon={faMagnifyingGlass}
+          />
+        </Link>
+
+        <p>Explorer</p>
+      </div>
+      <div className={styles.footerlogoCoeur}>
+        <Link href="/favorites">
+          <FontAwesomeIcon className={styles.useSelector} icon={faHeart} />
+        </Link>
+
+        <p>Favoris</p>
+      </div>
+    </footer>
   ) : (
-   
-      <Footer className={styles.footerStyleDesktop}>
-        <div>
-          <p>
-            Merci de noter qu'une caution sera exigée pour toute location de
-            matériel, qui sera utilisée en cas de casse. Consultez nos
-            conditions générales de location pour plus de détails
-          </p>
-        </div>
-        <div className={styles.footerDesktopLink}>
-          <p>Mentions Légales</p>
-          <p>CGV</p>
-          <p>Politique de confidentialité</p>
-          <p> Gestion des cookies</p>
-          <p> Nous contacter</p>
-        </div>
-      </Footer>
-    
+    <Footer className={styles.footerStyleDesktop}>
+      <div>
+        <p>
+          Merci de noter qu'une caution sera exigée pour toute location de
+          matériel, qui sera utilisée en cas de casse. Consultez nos conditions
+          générales de location pour plus de détails
+        </p>
+      </div>
+      <div className={styles.footerDesktopLink}>
+        <p>Mentions Légales</p>
+        <p>CGV</p>
+        <p>Politique de confidentialité</p>
+        <p> Gestion des cookies</p>
+        <p> Nous contacter</p>
+      </div>
+    </Footer>
   );
 
   return <div>{footerDisplay}</div>;
