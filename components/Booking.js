c66e26eb9d4e2c4875e804aa7407b44210956f08
Antony Merle <antony.merle@gmail.com>
2023-03-10 12:41:58 +0100

    Arrondi + deposit

--- components/Booking.js
+++ components/Booking.js
@@ -90,11 +90,15 @@ function Booking() {
             <p>Prix à la journée : {booking.dayPrice}€</p>
             <p>Prix hors taxes : {booking.dayPrice * recapNumberOfDays}€</p>
             <p>
-              Taxes : {Math.floor(booking.dayPrice * recapNumberOfDays * 0.2)}€
+              Taxes :{" "}
+              {Math.floor(booking.dayPrice * recapNumberOfDays * 0.2).toFixed(
+                2
+              )}
+              €
             </p>
             <p>Caution : {booking.deposit}€</p>
             <Divider className={styles.total} orientation="right">
-              Total : {total}€
+              Total : {total.toFixed(2)}€
             </Divider>
           </div>
           <div className={styles.btnContainer}>
